24c9e99e243640fd907b75155a3955e20b861d7b
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-03 02:26:19 +0800

    Update collocation_bigrams.py

--- collocation_bigrams.py
+++ collocation_bigrams.py
@@ -1,1 +1,12 @@
+import nltk
+from nltk.book() import *
+from nltk import bigrams
+
+
+x = list(bigrams(['hi','what','done','said','mean','can'])      // Separate array into 2 by 2
+     
+print(x)
+
+
+
 
