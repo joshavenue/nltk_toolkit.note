dd6f25c85b201ffe00f689d9465435d7d305dd91
joshg <30822255+joshavenue@users.noreply.github.com>
2017-09-25 23:12:58 +0800

    Update count_word_by_genre.py

--- nltk_note2/count_word_by_genre.py
+++ nltk_note2/count_word_by_genre.py
@@ -4,8 +4,8 @@ from nltk.corpus import brown
 print(brown.categories())
 
 genre_word = [(genre, word)                               ## You have Genre and Word
-				for genre in ['news', 'romance','hobbies']        ## For genre in the selected genre
-				for word in brown.words(categories=genre)]        ## For words in the selected genre
+	for genre in ['news', 'romance','hobbies']        ## For genre in the selected genre
+	for word in brown.words(categories=genre)]        ## For words in the selected genre
 
 
 cfd_1 = nltk.ConditionalFreqDist(genre_word)              ## Use a conditional freq distribution method for the variable
