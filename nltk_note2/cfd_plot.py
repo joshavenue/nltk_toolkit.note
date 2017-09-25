dc109632698bf5bbaabd47338191955bb030215a
joshg <30822255+joshavenue@users.noreply.github.com>
2017-09-25 21:25:16 +0800

    Create cfd_plot.py

---
+++ nltk_note2/cfd_plot.py
@@ -0,0 +1,12 @@
+import nltk
+from nltk.corpus import inaugural
+
+cfd = nltk.ConditionalFreqDist(
+		(target, fileid[:4])
+		for fileid in inaugural.fileids()
+		for w in inaugural.words(fileid)
+		for target in ['america', 'citizen']
+		if w.lower().startswith(target))
+
+cfd.plot()
+
